8054f38233f51dfbea5192ecf5b522f679a5d89e
Sparsh Rathi <sparshrathi1999@gmail.com>
2022-06-05 18:18:15 +0530

    header.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import '../style/Header.css';
+import LanguageIcon from '@mui/icons-material/Language';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Avatar } from '@mui/material';
 
 function Header() {
   return (
     <div className='header'>
 
-        <div className="header_icon">
-       <img src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-        alt="" />
-        </div>
+        <img 
+            className="header_icon" 
+            src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+            alt="" 
+        />
 
         <div className="header_center">
             <input type="text"/>
@@ -16,6 +21,12 @@ function Header() {
         </div>
 
         <div className="header_right">
+            <p>Become a host</p>
+            <LanguageIcon/>
+            <KeyboardArrowDownIcon/>
+            <Avatar alt="Sparsh Rathi"
+                src="https://avatars.githubusercontent.com/u/52618961?s=400&u=8a5555d9881c0caed7122cba4cdbc87339a2fdce&v=4"
+            />
         </div>
 
     </div>
